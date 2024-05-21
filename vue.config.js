53eb8bf404e49936171643bfa7965ce4d5032373
cc <cc@localhost.com>
2024-05-21 11:16:55 +0800

    webpack配置资源路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "https://html-static-resource.oss-cn-hangzhou.aliyuncs.com/graph_code" : "/",
   transpileDependencies: true,
   configureWebpack:{
     devtool: 'source-map',
