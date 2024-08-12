e18fa110248736697a4a980364098131cca6bbd6
YcM <3490395797@qq.com>
2024-08-12 19:20:49 +0800

    404页面的编写

---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<!-- src/views/NotFound.vue -->
+<template>
+    <div>
+        <br><br><br>
+      <img src="../../public/404.png" alt="404 Image"/>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFound'
+  }
+  </script>
+  
+  <style scoped>
+  /* 你的样式 */
+  /* img{
+    width: 256px;
+    height: auto;
+  } */
+  </style>
+  
